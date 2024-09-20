9677740b812a1271d7e6c062b66e97f74be8b3be
Andreas Reich <andreas@rerun.io>
2024-09-20 18:31:56 +0200

    DO NOT MERGE THIS ONE

--- docs/snippets/all/archetypes/video_manual_frames.py
+++ docs/snippets/all/archetypes/video_manual_frames.py
@@ -15,25 +15,79 @@ if len(sys.argv) < 2:
 rr.init("rerun_example_asset_video_manual_frames", spawn=True)
 
 # Log video asset which is referred to by frame references.
-rr.log("video_asset", rr.AssetVideo(path=sys.argv[1]), static=True)
+rr.set_time_seconds("video_time", 0.0)
+video_asset = rr.AssetVideo(path=sys.argv[1])
+rr.log("video_asset", video_asset, static=True)
 
-# Create two entities, showing the same video frozen at different times.
+
 rr.log(
-    "frame_at_start",
+    "delay0",
     rr.VideoFrameReference(
         timestamp=rr.components.VideoTimestamp(seconds=0.0),
         video_reference="video_asset",
     ),
 )
 rr.log(
-    "frame_at_one_second",
+    "delay1",
     rr.VideoFrameReference(
         timestamp=rr.components.VideoTimestamp(seconds=1.0),
         video_reference="video_asset",
     ),
 )
+rr.log(
+    "delay2",
+    rr.VideoFrameReference(
+        timestamp=rr.components.VideoTimestamp(seconds=2.0),
+        video_reference="video_asset",
+    ),
+)
+rr.log(
+    "delay3",
+    rr.VideoFrameReference(
+        timestamp=rr.components.VideoTimestamp(seconds=3.0),
+        video_reference="video_asset",
+    ),
+)
+
+# Send automatically determined video frame timestamps.
+frame_timestamps_ns = video_asset.read_frame_timestamps_ns()
+rr.send_columns(
+    "delay0",
+    times=[rr.TimeNanosColumn("video_time", frame_timestamps_ns)],
+    components=[rr.VideoFrameReference.indicator(), rr.components.VideoTimestamp.nanoseconds(frame_timestamps_ns)],
+)
+rr.send_columns(
+    "delay1",
+    times=[rr.TimeNanosColumn("video_time", frame_timestamps_ns)],
+    components=[
+        rr.VideoFrameReference.indicator(),
+        rr.components.VideoTimestamp.nanoseconds(frame_timestamps_ns - 1000000000.0),
+    ],
+)
+rr.send_columns(
+    "delay2",
+    times=[rr.TimeNanosColumn("video_time", frame_timestamps_ns)],
+    components=[
+        rr.VideoFrameReference.indicator(),
+        rr.components.VideoTimestamp.nanoseconds(frame_timestamps_ns - 2000000000.0),
+    ],
+)
+rr.send_columns(
+    "delay3",
+    times=[rr.TimeNanosColumn("video_time", frame_timestamps_ns)],
+    components=[
+        rr.VideoFrameReference.indicator(),
+        rr.components.VideoTimestamp.nanoseconds(frame_timestamps_ns - 3000000000.0),
+    ],
+)
+
 
 # Send blueprint that shows two 2D views next to each other.
 rr.send_blueprint(
-    rrb.Horizontal(rrb.Spatial2DView(origin="frame_at_start"), rrb.Spatial2DView(origin="frame_at_one_second"))
+    rrb.Grid(
+        rrb.Spatial2DView(origin="delay0"),
+        rrb.Spatial2DView(origin="delay1"),
+        rrb.Spatial2DView(origin="delay2"),
+        rrb.Spatial2DView(origin="delay3"),
+    )
 )
